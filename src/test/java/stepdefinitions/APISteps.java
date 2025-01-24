a8a56e93fea440ae809390f10a5ba704c025bcaa
sumittimus <mailtosumit@gmail.com>
2025-01-24 21:38:24 +1100

    Added and tested docker-compose functionality Performed changes to config.properties to reflect grid execution

--- src/test/java/stepdefinitions/APISteps.java
+++ src/test/java/stepdefinitions/APISteps.java
@@ -2,25 +2,56 @@ package stepdefinitions;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.Assert;
+
+import io.cucumber.java.After;
 import io.cucumber.java.en.*;
 
 public class APISteps {
-    private Response response;
 
-    @Given("I fetch store inventory")
-    public void getStoreInventory() {
-        response = RestAssured.get("https://petstore.swagger.io/v2/store/inventory");
-    }
+	// Using errorMessages to report errors while avoiding the build failure
+	private static List<String> errorMessages = new ArrayList<>();
+	private Response response;
 
-    @Then("I validate the response status code is {int}")
-    public void validateStatusCode(int expectedStatus) {
-        Assert.assertEquals(response.getStatusCode(), expectedStatus);
-    }
+	@Given("I fetch store inventory")
+	public void getStoreInventory() {
+		try {
+			response = RestAssured.get("https://petstore.swagger.io/v2/store/inventory");
+		} catch (Exception e) {
+			errorMessages.add("Error Details : " + e.getMessage());
+		}
 
-    @Then("I validate the 'pending' count")
-    public void validatePendingCount() {
-        int pendingCount = response.jsonPath().getInt("pending");
-        Assert.assertTrue(pendingCount > 0, "Pending count should be greater than 0");
-    }
+	}
+
+	@Then("I validate the response status code is {int}")
+	public void validateStatusCode(int expectedStatus) {
+		try {
+			Assert.assertEquals(response.getStatusCode(), expectedStatus);
+		} catch (Exception e) {
+			errorMessages.add("Different error code received : " + response.getStatusCode() + ". Fatal Error Details : "
+					+ e.getMessage());
+		}
+
+	}
+
+	@Then("I validate the 'pending' count")
+	public void validatePendingCount() {
+			int pendingCount = response.jsonPath().getInt("pending");
+			Assert.assertTrue(pendingCount > 0, "Pending count should be greater than 0");
+	}
+
+	@After
+	public void reportErrors() {
+		if (!errorMessages.isEmpty()) {
+			System.out.println("Test execution completed with the following errors:");
+			for (String error : errorMessages) {
+				System.out.println(error);
+			}
+			Assert.fail("Test execution completed with errors. Check logs for details.");
+		}
+	}
 }
